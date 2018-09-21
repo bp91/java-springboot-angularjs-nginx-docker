7b2d55e3d99aaf778f7523783fec490ac5034597
Bruno Pistone <brn.pistone@gmail.com>
2018-09-21 14:50:12 +0200

    #? update test

--- testwebapp_mvn/src/test/java/it/testwebapp_mvn/TestwebappMvnApplicationTests.java
+++ testwebapp_mvn/src/test/java/it/testwebapp_mvn/TestwebappMvnApplicationTests.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.google.common.collect.ImmutableMap;
@@ -17,6 +18,7 @@ import it.testwebapp_mvn.service.UserService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations="classpath:application-dev.properties")
 public class TestwebappMvnApplicationTests {
 	
 	@Autowired
